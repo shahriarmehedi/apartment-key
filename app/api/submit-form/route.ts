f9f0a937ad1c0c519f791ff0cf5fe188be78f7ae
shahriarmehedi <md.shahriar.mehedi@gmail.com>
2026-01-13 15:21:44 +0600

    Emails are now working on correct email 2

--- app/api/submit-form/route.ts
+++ app/api/submit-form/route.ts
@@ -46,7 +46,7 @@ export async function POST(request: Request) {
 
         const { data, error } = await resend.emails.send({
             from: 'Apartment Key <noreply@apartmentkey.com>',
-            to: ['Info@apartmentkey.com'],
+            to: ['info@apartmentkey.com'],
             subject: `New Form Submission from ${formData.firstName || ''} ${formData.lastName || ''}`,
             html: emailContent,
         });
